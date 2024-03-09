35fa85c19bd4cca9921f6bf1ed3ba83663a07fa5
gitdalisar <156739670+gitdalisar@users.noreply.github.com>
2024-03-08 21:17:11 -0800

    Adding git files and folder structure
    Added gitattributes and gitignore to project as well as a folder structure for the requisite files.

--- FactionFree.sql
+++
@@ -1,7 +0,0 @@
-/* 
-1. Ensure that you are logged into the Database instance
-2. Change to the acore_world database with USE acore_world;
-3. Run this using the SOURCE <path to .sql>; command
-*/
-UPDATE quest_template SET AllowableRaces = 1791 WHERE AllowableRaces = 1101;
-UPDATE quest_template SET AllowableRaces = 1791 WHERE AllowableRaces = 690;
