ed76ffb13cd0a9ab78be61ead08135a137c7680d
gitdalisar <156739670+gitdalisar@users.noreply.github.com>
2024-03-27 16:37:09 -0700

    Update Checker SQL
    move the GUID ranges down from 500m to 5m to correspond with new values

--- sql/world/EntryChecker_RUN_FIRST.sql
+++ sql/world/EntryChecker_RUN_FIRST.sql
@@ -18,7 +18,7 @@ To run this script:
 /*Check to see if entry 500000 and 500001 are free to add the creature templates for the Teleport Network*/
 SELECT `entry`, `name`, `subname`, `faction`, `npcflag` FROM `acore_world`.`creature_template` WHERE `entry` IN (500000, 500001);
 /*Check to see if GUIDs 500000000-500000019 are free to add the creature objects for the Teleport Network*/
-SELECT `guid`, `id1`, `map`, `zoneId`, `areaId` FROM `acore_world`.`creature` WHERE `guid` BETWEEN 500000000 AND 500000019;
+SELECT `guid`, `id1`, `map`, `zoneId`, `areaId` FROM `acore_world`.`creature` WHERE `guid` BETWEEN 5000000 AND 5000019;
 /*Check to see if entry 500000, 500001, and 500003 are free to add the broadcast text entries for the Teleport Network*/
 SELECT `ID`, `LanguageID`, `MaleText`, `FemaleText` FROM `acore_world`.`broadcast_text` WHERE `ID` IN (500000, 500001, 500003);
 /*Check to see if entry 500000, 500001, and 500003 are free to add the npc text entries for the Teleport Network*/
@@ -26,7 +26,7 @@ SELECT `ID`, `text0_0`, `text0_1`, `lang0` FROM `acore_world`.`npc_text` WHERE `
 /*Check to see if entry 500000-500004 are free to add the gameobject templates for the Teleport Network*/
 SELECT `entry`, `type`, `displayId`, `name` FROM `acore_world`.`gameobject_template` WHERE `entry` BETWEEN 500000 AND 500004;
 /*Check to see if GUIDs 500000000-500000049 are free to add the gameobjects for the Teleport Network*/
-SELECT `guid`, `id`, `map`, `zoneId`, `areaId` FROM `acore_world`.`gameobject` WHERE `guid` BETWEEN 500000000 AND 500000049;
+SELECT `guid`, `id`, `map`, `zoneId`, `areaId` FROM `acore_world`.`gameobject` WHERE `guid` BETWEEN 5000000 AND 5000049;
 
 /*AGAIN: Please note that if you have any results back from these queries you will need to modify the FactionFree.sql
 to use new and free entry, id, or GUID values.*/
